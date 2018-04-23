2e5d561a5df960833a3aca13e83ef4a44958ea1b
Alexander Pitkin <peleccom@gmail.com>
2018-04-23 11:48:47 +0300

    Fix git as command

--- git-as.sh
+++ git-as.sh
@@ -3,6 +3,4 @@
 # https://noamlewis.wordpress.com/2013/01/24/git-admin-an-alias-for-running-git-commands-as-a-privileged-ssh-identity/
 set -e
 set -u
-set -x
-set -v
 SSH_KEYFILE=$1 GIT_SSH_COMMAND=${BASH_SOURCE%/*}/ssh-as.sh exec git "${@:2}"
--- ssh-as.sh
+++ ssh-as.sh
@@ -2,6 +2,4 @@
 # https://noamlewis.wordpress.com/2013/01/24/git-admin-an-alias-for-running-git-commands-as-a-privileged-ssh-identity/
 set -e
 set -u
-set -x
-set -v
-exec ssh ${SSH_KEYFILE+-i "$SSH_KEYFILE"} "$@"
+exec ssh -o IdentitiesOnly=yes ${SSH_KEYFILE+-i "$SSH_KEYFILE"} "$@"
