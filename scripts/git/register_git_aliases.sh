7efe928528f845fbcdae6b1b3168a2423afb9a69
Alexander Pitkin <peleccom@gmail.com>
2018-04-24 15:23:14 +0300

    Add git aliases

--- scripts/git/register_git_aliases.sh
+++ scripts/git/register_git_aliases.sh
@@ -4,6 +4,8 @@ set -u
 
 # https://noamlewis.wordpress.com/2013/01/24/git-admin-an-alias-for-running-git-commands-as-a-privileged-ssh-identity/ 
 # Run git commands as the SSH identity provided by the keyfile ~/.ssh/admin
+git config --global --remove-section alias
 git config --global alias.my \!"$HOME/config_scripts/git-as.sh ~/.ssh/my/id_rsa"
 git config --global alias.work \!"$HOME/config_scripts/git-as.sh ~/.ssh/work/id_rsa"
-
+git config --global alias.co "checkout"
+git config --global alias.hist "log --pretty=format:\"%h %ad | %s%d [%an]\" --graph --date=short"
