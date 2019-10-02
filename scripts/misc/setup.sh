7e71bc8123694a4ad57ab3a07c91936eb8aa715b
Alexander Pitkin <peleccom@gmail.com>
2019-10-02 11:11:19 +0300

    Install powerline font for vscode

--- scripts/misc/setup.sh
+++ scripts/misc/setup.sh
@@ -25,4 +25,7 @@ ln -sf `readlink -f ../../home/.config/sublime-text-3/Packages/User/` ~/.config/
 mkdir -p ~/.config/Code/User
 rm -rf ~/.config/Code/User/settings.json
 ln -sf `readlink -f ../../home/.config/Code/User/settings.json` ~/.config/Code/User/
+wget https://github.com/abertsch/Menlo-for-Powerline/raw/master/Menlo%20for%20Powerline.ttf -P ~/.fonts
 
+#update font cache
+fc-cache -vf ~/.fonts
