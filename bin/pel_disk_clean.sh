587113310f1e46bc887937f0eeb552b78176c26a
Aliaksandr Pitkin <apitkin@two.inc>
2025-09-10 23:35:22 +0300

    AAA

--- bin/pel_disk_clean.sh
+++ bin/pel_disk_clean.sh
@@ -8,11 +8,11 @@ sudo apt-get clean
 sudo apt-get autoremove -y
 
 # Clean up snap
-sudo snap set system refresh.retain=2
-LANG=en_US.UTF-8 snap list --all | awk '/disabled/{print $1, $3}' |
-    while read -r snapname revision; do
-        sudo snap remove "$snapname" --revision="$revision"
-    done
+#sudo snap set system refresh.retain=2
+#LANG=en_US.UTF-8 snap list --all | awk '/disabled/{print $1, $3}' |
+#    while read -r snapname revision; do
+#        sudo snap remove "$snapname" --revision="$revision"
+#    done
 
 # Clean up journal
 sudo journalctl --vacuum-time=3d
@@ -21,5 +21,5 @@ sudo journalctl --vacuum-time=3d
 rm -rf "$HOME/.cache/thumbnails"/*
 
 # Clean up old kernels
-dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d' | 
-    xargs sudo apt-get -y purge
+#dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d' | 
+#    xargs sudo apt-get -y purge
